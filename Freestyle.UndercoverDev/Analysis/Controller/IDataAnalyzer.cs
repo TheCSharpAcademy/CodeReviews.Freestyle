1a471d5f266adc11663cae3b0ade2d0f5be6af9e
N-Endy <okafornel@gmail.com>
2024-10-04 23:09:35 +0100

    Add code to filter predictions for analyzer

---
+++ Freestyle.UndercoverDev/Analysis/Controller/IDataAnalyzer.cs
@@ -0,0 +1,12 @@
+using Data.Models;
+
+namespace Analysis.Controller;
+public interface IDataAnalyzer
+{
+    IEnumerable<MatchData> StraightWin();
+    IEnumerable<MatchData> OverTwoGoals();
+    IEnumerable<MatchData> UnderTwoGoals();
+    IEnumerable<MatchData> Draw();
+    IEnumerable<MatchData> BothTeamsScore();
+    IEnumerable<MatchData> OverThreeGoals();
+}
